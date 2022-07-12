a2b1680b5b39358c2d30b3724fd0ea4f47e670c9
Tripoli80 <Tripoli80@gmail.com>
2022-07-12 19:37:29 +0200

    infinity +fix

--- src/js/main.js
+++ src/js/main.js
@@ -47,9 +47,14 @@ function search(e) {
 }
 function loader(e) {
   e.preventDefault();
-  myGallery.pageincrement();
+  if (myGallery.checkTotalHits()) {
+    myGallery.pageincrement();
 
-  myGallery.axiosIMG().then(getNewCards);
+    myGallery.axiosIMG().then(getNewCards);
+  } else {
+    btnDisenable(refs.btnLoadMore);
+    Notify.info(info);
+  }
 }
 
 function getNewCards({ hits, totalCount }) {
@@ -131,5 +136,33 @@ function btnDisenable(btn) {
     setTimeout(() => {
       btn.classList.toggle('none');
     }, 1500);
+  }
+}
+
+window.addEventListener('scroll', checkPosition);
+window.addEventListener('resize', checkPosition);
+
+function checkPosition(e) {
+  // Нам потребуется знать высоту документа и высоту экрана:
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+
+  // Они могут отличаться: если на странице много контента,
+  // высота документа будет больше высоты экрана (отсюда и скролл).
+
+  // Записываем, сколько пикселей пользователь уже проскроллил:
+  const scrolled = window.scrollY;
+
+  // Обозначим порог, по приближении к которому
+  // будем вызывать какое-то действие.
+  // В нашем случае — четверть экрана до конца страницы:
+  const threshold = height - screenHeight / 4;
+
+  // Отслеживаем, где находится низ экрана относительно страницы:
+  const position = scrolled + screenHeight;
+
+  if (position >= threshold) {
+    loader(e);
+    // Если мы пересекли полосу-порог, вызываем нужное действие.
   }
 }
